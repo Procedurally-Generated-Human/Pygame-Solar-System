12ac1f89da637d971337c23b10947b551e6030ad
Procedurally-Generated-Human <87566207+Procedurally-Generated-Human@users.noreply.github.com>
2021-07-29 22:12:44 +0430

    Update solar-system.py

--- solar-system.py
+++ solar-system.py
@@ -11,18 +11,21 @@ def draw_stars(list2d):
 
 
 class Planet():
-    def __init__(self, radius,speed, color, x_radius, y_radius):
+    def __init__(self, radius,speed, color, x_radius, y_radius, rings):
         self.radius = radius
         self.speed = speed
         self.degree = 0
         self.color = color
         self.x_radius = x_radius
         self.y_radius = y_radius
+        self.rings = rings
     
     def draw(self):
             x1 = int(math.cos(self.degree * 2 * math.pi/360) * self.x_radius) + width/2
             y1 = int(math.sin(self.degree * 2 * math.pi/360) * self.y_radius) + height/2
             pygame.draw.circle(window, self.color, (x1, y1), self.radius)
+            if self.rings == True:
+                pygame.draw.line(window, white,(x1 - 40, y1),(x1 + 40, y1) ,3)
 
     def draw_orbit(self):
             pygame.draw.ellipse(window, white, [width/2 - self.x_radius,height/2-self.y_radius,2*(self.x_radius),2*(self.y_radius)] ,1)
@@ -72,14 +75,14 @@ stars = []
 create_stars(stars, 100, width, height)
 sun = Sun(40,width/2,height/2,yellow)
 
-mercury = Planet(4.8,2.35,grey, 90,45)
-venus = Planet(9.5,1.8,orange, 140,60)
-earth = Planet(10,1.5,mid_blue, 180,80)
-mars = Planet(5.3,1.2,red, 230,100)
-jupiter = Planet(30,0.65,creamy, 300, 160)
-saturn = Planet(24,0.49,dark_orange, 400, 220)
-uranus = Planet(18,0.34,light_blue, 500, 290)
-neptune = Planet(15.8,0.27,blue, 580, 330)
+mercury = Planet(4.8,2.35,grey, 90,45, False)
+venus = Planet(9.5,1.8,orange, 140,60, False)
+earth = Planet(10,1.5,mid_blue, 180,80, False)
+mars = Planet(5.3,1.2,red, 230,100, False)
+jupiter = Planet(30,0.65,creamy, 300, 160, False)
+saturn = Planet(24,0.49,dark_orange, 400, 220, True)
+uranus = Planet(18,0.34,light_blue, 500, 290, False)
+neptune = Planet(15.8,0.27,blue, 580, 330, False)
 planets = [ mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
 
 
@@ -105,4 +108,4 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+    main()
